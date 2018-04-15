79a2cf5ac4fefbe9c5eaa077f7d013f2bf883a4f
visrahane <rahane@usc.edu>
2018-04-15 16:28:16 -0700

    add volley impl to fetch search result

--- app/src/main/java/com/vis/entertainment/activity/MainActivity.java
+++ app/src/main/java/com/vis/entertainment/activity/MainActivity.java
@@ -1,22 +1,26 @@
 package com.vis.entertainment.activity;
 
+import android.net.Uri;
 import android.support.design.widget.TabLayout;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.RadioGroup;
+import android.widget.Spinner;
+import android.widget.TextView;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.vis.entertainment.R;
-import com.vis.entertainment.fragment.FragmentOne;
-import com.vis.entertainment.fragment.FragmentThree;
-import com.vis.entertainment.fragment.FragmentTwo;
 import com.vis.entertainment.fragment.ViewPagerAdapter;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -24,16 +28,22 @@ public class MainActivity extends AppCompatActivity {
     private ViewPager viewPager;
     private ViewPagerAdapter viewPagerAdapter;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         viewPager = (ViewPager) findViewById(R.id.viewPager);
-        viewPagerAdapter = new ViewPagerAdapter(getSupportFragmentManager(),this);
+        viewPagerAdapter = new ViewPagerAdapter(getSupportFragmentManager(), this);
         viewPager.setAdapter(viewPagerAdapter);
         tabLayout = (TabLayout) findViewById(R.id.tabs);
         tabLayout.setupWithViewPager(viewPager);
+
+
+
     }
+
+
 
 }
--- app/src/main/java/com/vis/entertainment/fragment/SearchFragment.java
+++ app/src/main/java/com/vis/entertainment/fragment/SearchFragment.java
@@ -1,19 +1,109 @@
 package com.vis.entertainment.fragment;
 
+import android.app.ProgressDialog;
+import android.net.Uri;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.RadioGroup;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.vis.entertainment.R;
 /**
  * Created by Vis on 13-04-2018.
  */
 
 public class SearchFragment extends Fragment {
+
+    private  View view;
+    private TextView keywordTxt;
+    private TextView distanceTxt;
+    private AutoCompleteTextView locationTxt;
+    private RadioGroup fromRadioGrp;
+    private Spinner categorySpinner;
+    private RequestQueue requestQueue;
+    private ProgressBar progressBar;
+    private ProgressDialog progress;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.input_form, container, false);
+       view = inflater.inflate(R.layout.input_form, container, false);
+
+        init();
+        Button searchBtn = (Button) view.findViewById(R.id.searchBtn);
+        searchBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //progressBar.setVisibility(View.VISIBLE);
+                progress.show();
+                getResults();
+            }
+        });
         return view;
+    }
+    private void init() {
+        keywordTxt=  view.findViewById(R.id.keywordTxt);
+        distanceTxt= (TextView) view.findViewById(R.id.distanceTxt);
+        locationTxt= (AutoCompleteTextView) view.findViewById(R.id.locationTxt);
+        categorySpinner= (Spinner) view.findViewById(R.id.categoryDropDown);
+        fromRadioGrp= (RadioGroup) view.findViewById(R.id.fromRadioGroup);
+        requestQueue= Volley.newRequestQueue(this.getContext().getApplicationContext());
+        progressBar=view.findViewById(R.id.resultsProgressBar);
+
+        //
+
+        progress=new ProgressDialog(this.getContext(),R.style.AppCompatAlertDialogStyle);
+        progress.setMessage("Fetching results");
+        progress.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+        progress.setIndeterminate(true);
+
+    }
+
+    private void getResults() {
+        //make a call to the server and fetch result
+        String url=getResources().getString(R.string.searchResultUri);
+        Uri builtUri = Uri.parse(url)
+                .buildUpon()
+                .appendQueryParameter("keyword",keywordTxt.getText().toString() )
+                .appendQueryParameter("category", categorySpinner.getSelectedItem().toString())
+                .appendQueryParameter("distance", distanceTxt.getText().toString().isEmpty()?"10":distanceTxt.getText().toString())
+                .appendQueryParameter("location", locationTxt.getText().toString().isEmpty()?"here":locationTxt.getText().toString())
+                .appendQueryParameter("latitude","34.029653")
+                .appendQueryParameter("longitude","-118.283130")
+                .build();
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, builtUri.toString(),
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        // Display the first 500 characters of the response string.
+                        progressBar.setVisibility(View.GONE);
+                        progress.dismiss();
+                        Log.d("Search Response is: ", response);
+
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                //error Message
+                progressBar.setVisibility(View.VISIBLE);
+                Log.d("Search Response is: ", error.toString());
+            }
+        });
+
+// Add the request to the RequestQueue.
+        requestQueue.add(stringRequest);
     }
 }
